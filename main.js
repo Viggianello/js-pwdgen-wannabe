2fd471899fb3c449424b1beb4ef0946d662f1c0f
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-17 16:02:16 +0200

    esercizio 17aprile javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+// chiedere all'utente il suo nome
+var nome_utente = prompt('Scrivi il tuo nome:');
+
+// chiedere all'utente il suo cognome
+var cognome_utente = prompt('Scrivi il tuo cognome:');
+
+// chiedere all'utente il suo colore preferito
+var colore_preferito_utente = prompt('Scrivi il tuo colore preferito:');
+
+// visualizzare output finale
+document.writeln('Ecco la tua insicurissima password: ' + nome_utente + cognome_utente +     colore_preferito_utente + 20)
